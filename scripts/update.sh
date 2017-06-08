f4672d2990324aa1ae54edfdb26d324130f40a2c
Yive <admin@yive.me>
2017-06-07 23:51:24 -0700

    Create update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-server=( arkhamnetwork.org mc.arkhamnetwork.org play.arkhamnetwork.org playmc.mx primemc.org play.primemc.org muxcraft.eu minetime.com luckyprison.com play.luckyprison.com meepcraft.com itsjerryandharry.com play.itsjerryandharry.com ecocitycraft.com mc.ecocitycraft.com play.ecocitycraft.com eccgamers.com mc.eccgamers.com play.eccgamers.com aemservers.net mc.aemservers.net play.aemservers.net cosmicpvp.com cosmicpvp.me proxypipe.cosmicpvp.com play.manacube.com mc.minevast.com yaymc.com skyblocky.com mc.skyblocky.com gotpvp.com play.gotpvp.com pvp.desteria.com mc.cathalcraft.com play.lemoncloud.org )
+server=( arkhamnetwork.org mc.arkhamnetwork.org play.arkhamnetwork.org playmc.mx primemc.org play.primemc.org muxcraft.eu pvp.muxcraft.eu minetime.com luckyprison.com play.luckyprison.com meepcraft.com itsjerryandharry.com play.itsjerryandharry.com ecocitycraft.com mc.ecocitycraft.com play.ecocitycraft.com eccgamers.com mc.eccgamers.com play.eccgamers.com aemservers.net mc.aemservers.net play.aemservers.net cosmicpvp.com cosmicpvp.me proxypipe.cosmicpvp.com play.manacube.com mc.minevast.com yaymc.com skyblocky.com mc.skyblocky.com gotpvp.com play.gotpvp.com pvp.desteria.com mc.cathalcraft.com play.lemoncloud.org )
 for i in "${server[@]}"
 do
     echo "-------------------------------------------------"
